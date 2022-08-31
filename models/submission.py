6bee179367b2a914cb93e7daf870beb46724a06b
Caio Santoro dos Santos <61211407+csantbr@users.noreply.github.com>
2022-08-31 19:39:51 -0300

    Fix submission.py error on uuid generator.

--- models/submission.py
+++ models/submission.py
@@ -7,7 +7,7 @@ from uuid import UUID, uuid4
 class Submission(Base):
     __tablename__ = 'submissions'
 
-    id: UUID = Column(PG_UUID(as_uuid=True), default=uuid4(), primary_key=True, index=True)
+    id: UUID = Column(PG_UUID(as_uuid=True), default=uuid4, primary_key=True, index=True)
     problem_id: UUID = Column(PG_UUID(as_uuid=True), ForeignKey('problems.id'), nullable=False)
     language_type: str = Column(String, nullable=False)
     content: str = Column(String, nullable=False)
